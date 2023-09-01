f3aada6abec1a62298faefecd56be6b2e1e5f451
adam swenson <adam.swenson@csun.edu>
2023-09-01 15:41:28 -0700

    worst sysadmin in world

--- python-scripts/ResolutionManager/Repositories/DocumentRepository.py
+++ python-scripts/ResolutionManager/Repositories/DocumentRepository.py
@@ -8,7 +8,7 @@ import os.path
 # from google.oauth2.credentials import Credentials
 # from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+# from googleapiclient.errors import HttpError
 
 from ResolutionManager.API.CredentialsManager import CredentialsManager
 import ResolutionManager.environment as env
