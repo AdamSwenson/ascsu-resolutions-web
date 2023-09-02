1adb5b9ab6f8375c74d86a20c940a14092db371b
adam swenson <adam.swenson@csun.edu>
2023-09-01 17:42:00 -0700

    worst sysadmin in world

--- python-scripts/executables/web_create_resolution_from_template.py
+++ python-scripts/executables/web_create_resolution_from_template.py
@@ -5,6 +5,8 @@ import os
 
     # sys.path.append("/Users/ars62917/Dropbox/ResolutionManager")
 sys.path.append("/Users/ars62917/Dropbox/ResolutionManagerWeb/python-scripts")
+sys.path.append("/home/ascsuadam-swensoncom/ascsu.adam-swenson.com/python-scripts/ResolutionManager")
+sys.path.append("/home/ascsuadam-swensoncom/ascsu.adam-swenson.com/python-scripts")
 
 
     # from ResolutionManager import environment as env
