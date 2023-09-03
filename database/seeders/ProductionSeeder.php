5370f730f562144cb5e96e781097eb6f7569f3fd
adam swenson <adam.swenson@csun.edu>
2023-09-03 13:07:18 -0700

    removed plenary from production seeder

--- database/seeders/ProductionSeeder.php
+++ database/seeders/ProductionSeeder.php
@@ -16,14 +16,14 @@ class ProductionSeeder extends Seeder
     {
 
 
-        $p = new Plenary([
-            'thursday_date' => Carbon::today(),
-
-            'first_reading_folder_id' =>'1-U0SBib3UmfiXybzARUViIEmwj3mLhY5',
-            'plenary_folder_id'=>'1ITs5N1qpTbqVqAhALrxqSsDwiSKsnSj5',
-            'is_current' => true
-        ]);
-        $p->save();
+//        $p = new Plenary([
+//            'thursday_date' => Carbon::today(),
+//
+//            'first_reading_folder_id' =>'1-U0SBib3UmfiXybzARUViIEmwj3mLhY5',
+//            'plenary_folder_id'=>'1ITs5N1qpTbqVqAhALrxqSsDwiSKsnSj5',
+//            'is_current' => true
+//        ]);
+//        $p->save();
 
         $this->call(CommitteeTableSeeder::class);
 
