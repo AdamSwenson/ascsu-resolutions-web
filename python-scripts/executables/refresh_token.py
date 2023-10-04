86aadd004ffbb71e4f83f3edf193196847c43237
adam swenson <adam.swenson@csun.edu>
2023-10-04 16:51:54 -0700

    ran refresh token

--- python-scripts/executables/refresh_token.py
+++ python-scripts/executables/refresh_token.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.append("/Users/ars62917/Dropbox/ResolutionManagerWeb/python-scripts")
 sys.path.append("/home/ascsuadam-swensoncom/ascsu.adam-swenson.com/python-scripts/ResolutionManager")
 sys.path.append("/home/ascsuadam-swensoncom/ascsu.adam-swenson.com/python-scripts")
+sys.path.append("/Users/ars62917/Dropbox/ResolutionManagerWeb/python-scripts")
 
 from ResolutionManager.DAO.DAO import MySqlDao
 from ResolutionManager.Repositories.ResolutionRepository import ResolutionRepository
